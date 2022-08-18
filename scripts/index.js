516271197a71e385d067048e71623c485dac02a9
DESKTOP-SJJ9J08\Bozhena <luskavetsbozhena@gmail.com>
2022-08-18 11:29:38 +0300

    upgrade signIn & signUp functions, form validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,44 @@
 const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const checkboxInput = document.getElementById('checkbox');
-let button = document.getElementById('btn');
+const button = document.getElementById('btn');
+const signIn = document.getElementById('signIn');
+const signUp = document.getElementById('signUp');
+const form = document.getElementById('form');
+const defaultStyles = signIn.classList.add('active');
+
+usernameInput.addEventListener('keyup', isEmpty);
+passwordInput.addEventListener('keyup', isEmpty);
+checkboxInput.addEventListener('keyup', isEmpty);
+checkboxInput.addEventListener('click', isEmpty);
+
+signIn.addEventListener('click', () => {
+    signIn.classList.add('active');
+    signUp.classList.remove('active');
+    form.reset();
+    isEmpty();
+})
+
+signUp.addEventListener('click', () => {
+    signUp.classList.add('active');
+    signIn.classList.remove('active');
+    form.reset();
+    isEmpty();
+})
 
 function isEmpty() {
     let username = usernameInput.value;
     let password = passwordInput.value;
 
-    if (username != '' && password != '' && checkboxInput.checked == true) {
-        button.removeAttribute('disabled');
-        button.style.backgroundColor = '#F3CB4A';
-    }
+    const isDisabled = !(username?.length && password?.length && checkboxInput.checked);
+    button.disabled = isDisabled;
 }
 
 function getInputValue() {
     let username = usernameInput.value;
     let password = passwordInput.value;
-    let obj = {}
+    let obj = {};
     obj.username = username;
-    obj.password = password
-    console.log(obj)
+    obj.password = password;
+    console.log(obj);
 }
